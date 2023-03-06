ea887b111b49cf32deee548d800d7bc885e3a3a1
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-03-06 14:31:12 -0400

    gg

--- page/src/components/Estagios/Step1.tsx
+++ page/src/components/Estagios/Step1.tsx
@@ -3,8 +3,8 @@ import Formulario from '../Formulario'
 export default function Step1(){
     return(
         <div className='divP_setp1'>
-            <h1>Personal info</h1>
-            <p>Please Provide your name , email address , and phone number</p>
+            <h1 className='Step_1h1'>Personal info</h1>
+            <p className='Setp_1p'>Please Provide your name , email address , and phone number</p>
             <Formulario/>
         </div>
         
